9133f8c3a479009607a4a637bc4eb982fdfd9262
pro100kryto <pro100kryto.original@gmail.com>
2021-04-29 12:10:05 -0300

    Change packets id

--- src/main/java/com/gvargame/server/modules/simplebattle/packet/PacketId.java
+++ src/main/java/com/gvargame/server/modules/simplebattle/packet/PacketId.java
@@ -4,25 +4,27 @@ public final class PacketId {
     public static final class Server{
         public static final short WRONG = 0;
         public static final short PONG = 1;
-        public static final short MSG_ERROR = 2;
-        public static final short PLAYER_POS = 3;
-        public static final short SPAWN = 4;
-        public static final short DEAD = 5;
-        public static final short SHOOT_VOID = 6;
-        public static final short SHOOT = 7;
-        public static final short PLAYERS_LIST = 8;
+        public static final short PING = 2;
+        public static final short MSG_ERROR = 3;
+        public static final short PLAYER_POS = 4;
+        public static final short SPAWN = 5;
+        public static final short DEAD = 6;
+        public static final short SHOOT_VOID = 7;
+        public static final short SHOOT = 8;
+        public static final short PLAYERS_LIST = 9;
     }
 
     public static final class Client{
         public static final short WRONG = 0;
         public static final short PING = 1;
-        public static final short BODY_POS = 2;
-        public static final short GUN_POS = 3;
-        public static final short SPAWN_REQ = 4;
-        public static final short KILL_SELF = 5;
-        public static final short SHOOT_VOID = 6;
-        public static final short SHOOT_WALL = 7;
-        public static final short SHOOT_PLAYER = 8;
-        public static final short PLAYERS_LIST = 9;
+        public static final short PONG = 2;
+        public static final short BODY_POS = 3;
+        public static final short GUN_POS = 4;
+        public static final short SPAWN_REQ = 8;
+        public static final short KILL_SELF = 9;
+        public static final short SHOOT_VOID = 7;
+        public static final short SHOOT_WALL = 8;
+        public static final short SHOOT_PLAYER = 9;
+        public static final short PLAYERS_LIST = 10;
     }
 }
